1013e42c54f0a92e8d320e51750cb2183df031de
Wolf Pinz <wolfpinz@gmail.com>
2022-05-19 17:01:21 +0200

    Routing, Controller, Views for bookmarks - add/create new bookmark - done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  # comment
+  # movie_id
+  # list_id
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
