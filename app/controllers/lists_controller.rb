b8380322d03475868903d8aacd0bcef8cfded0d7
Wolf Pinz <wolfpinz@gmail.com>
2022-05-19 15:48:10 +0200

    Routing, Controller, Views for Lists done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,24 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
